880b7c2f90aeaa5a2311cbc35b1c4a13f8f58e7b
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-01-19 16:49:32 +0900

    モデル・テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :nickname,             presence: true
   validates :firstname,            presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :lastname,             presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
