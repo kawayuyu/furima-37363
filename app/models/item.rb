1325e530b067aaccd06b6fc4d912808cadcfd7b0
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-01-20 16:57:26 +0900

    商品情報保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_fee
   belongs_to :prefecture
-  belongs_to :scheduled_delivery_id
+  belongs_to :scheduled_delivery
 
   with_options presence: true do
     validates :name
@@ -21,7 +22,7 @@ class Item < ApplicationRecord
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
-    validates :steatus_id
+    validates :status_id
     validates :delivery_fee_id
     validates :prefecture_id
     validates :scheduled_delivery_id
