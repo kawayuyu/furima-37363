ecdd1f38f7700615d4be8f06967ef192444c0cfd
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-01-21 18:11:52 +0900

    Rubocop修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Item, type: :model do
         it 'userが紐付いていなければ出品できないこと' do
           @item.user = nil
           @item.valid?
-          expect(@item.errors.full_messages).to include("User must exist")
+          expect(@item.errors.full_messages).to include('User must exist')
         end
 
         it 'imageが空欄だと出品できないこと' do
