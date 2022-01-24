f5c7a5652eeca5073a3ea0b41cd03b355f97570d
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-01-24 16:21:49 +0900

    詳細表示パス記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,20 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if  @item.user_id != current_user.id 
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to Item.find(params[:id])
     else
       render :edit
     end
@@ -44,4 +42,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :image, :price, :detail, :category_id, :status_id, :delivery_fee_id, :prefecture_id,
                                  :scheduled_delivery_id).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
